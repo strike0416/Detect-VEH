1b73336a15b72d24b7cb353a39617525de0028cb
Strike <stroke0416@naver.com>
2026-05-05 00:48:43 +0900

    Update memory.h

--- handler_check/memory.h
+++ handler_check/memory.h
@@ -6,13 +6,13 @@
 
 namespace memory
 {
-	bool is_readable(std::uint64_t start, size_t size);
+	bool is_readable(uintptr_t start, size_t size);
 
 	//BYTE, wild card
-	std::vector<std::pair<std::uint8_t, bool>> string_to_pattern(const char* pattern);
+	std::vector<std::pair<uint8_t, bool>> string_to_pattern(const char* pattern);
 
 	//pattern scan for memory region
-	std::uint64_t pattern_scan(std::uint64_t start, std::uint64_t size, const char* pattern);
+	uintptr_t pattern_scan(uintptr_t start, size_t size, const char* pattern);
 }
 
 #endif
